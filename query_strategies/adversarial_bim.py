bf29f3fd7b40d520ddc317c351c1b8b632443dac
mixiancmx <83197963+mixiancmx@users.noreply.github.com>
2021-11-22 16:05:06 +0800

    Update adversarial_bim.py

--- query_strategies/adversarial_bim.py
+++ query_strategies/adversarial_bim.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from .strategy import Strategy
+# from https://github.com/JordanAsh/badge
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
