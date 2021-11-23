80974200c81a9ff15e0014648f4ce51c5d95d2ed
mixiancmx <83197963+mixiancmx@users.noreply.github.com>
2021-11-23 20:34:01 +0800

    Update ssl_rand.py

--- query_strategies/ssl_rand.py
+++ query_strategies/ssl_rand.py
@@ -2,8 +2,8 @@ import numpy as np
 from .semi_strategy import semi_Strategy
 
 class ssl_Random(semi_Strategy):
-    def __init__(self, X, Y, idxs_lb, net, handler, args):
-        super(ssl_Random, self).__init__(X, Y, idxs_lb, net, handler, args)
+    def __init__(self, X, Y, X_te, Y_te, idxs_lb, net, handler, args):
+        super(ssl_Random, self).__init__(X, Y, X_te, Y_te, idxs_lb, net, handler, args)
 
     def query(self, n):
         inds = np.where(self.idxs_lb==0)[0]
