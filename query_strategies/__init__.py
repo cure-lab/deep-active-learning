d5d4019d61609bbda47c275b141e7afb5a4fb55c
AaronWANG-ZX <91113854+AaronWANG-ZX@users.noreply.github.com>
2022-03-30 14:30:42 +0800

    Update __init__.py

--- query_strategies/__init__.py
+++ query_strategies/__init__.py
@@ -30,4 +30,9 @@ from .ssl_rand import ssl_Random
 from .ssl_diff2augkmeans import ssl_Diff2AugKmeans
 from .ssl_diff2augdirect import ssl_Diff2AugDirect
 from .ssl_consistency import ssl_Consistency
-# from .uda import ssl_UDA
+
+# SSL + AL
+from .ssl_uda_rs import uda_rs
+from .ssl_fixmatch_rs import fixmatch_rs
+from .ssl_flexmatch_rs import flexmatch_rs
+from .ssl_pseudolabel_rs import pseudolabel_rs
